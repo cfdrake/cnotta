05dbbcea75833e7b4858bf0a56e5b6f6d6e17e52
Colin Drake <github@accounts.colindrake.me>
2026-09-11 14:25:37 -0500

    Initial Commit

---
+++ Cnotta/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Cnotta
+//
+//  Created by Colin Drake on 9/11/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ CnottaTests/CnottaTests.swift
@@ -0,0 +1,17 @@
+//
+//  CnottaTests.swift
+//  CnottaTests
+//
+//  Created by Colin Drake on 9/11/26.
+//
+
+import Testing
+@testable import Cnotta
+
+struct CnottaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
