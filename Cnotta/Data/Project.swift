7848cf80317e9cbdec9b17793ecfb60a6f321cab
Colin Drake <github@accounts.colindrake.me>
2026-09-17 17:21:15 -0500

    Wrap Project in schema

--- Cnotta/Data/Project.swift
+++ Cnotta/Data/Project.swift
@@ -8,44 +8,58 @@
 import Foundation
 import SwiftData
 
-/// Represents a project with live counter and other associated metadata. A SwiftData model.
-@Model
-final class Project: Identifiable {
-    
-    // MARK: - Properties
-    
-    /// Unique identifier for the project.
-    var id: UUID
-    
-    /// Project's name.
-    var name: String
-    
-    /// Datetime the project was created.
-    var created: Date
-    
-    /// Datetime the project was last updated.
-    var modified: Date
-    
-    /// Count value of the project.
-    var count: Int
-    
-    /// Color for the project.
-    var color: Int
-    
-    // MARK: - Initialization
+typealias Project = ProjectsSchemaV1.Project
 
-    init(id: UUID = UUID(), name: String, created: Date = Date(), modified: Date = Date(), count: Int = 0, color: Int) {
-        // Sanity check for proper values.
-        assert(count >= 0)
-        assert(color >= 0x000000 && color <= 0xffffff)
-        
-        self.id = id
-        self.name = name
-        self.created = created
-        self.modified = modified
-        self.count = count
-        self.color = color
+enum ProjectsSchemaV1: VersionedSchema {
+    
+    static var versionIdentifier: Schema.Version = Schema.Version(1, 0, 0)
+    
+    static var models: [any PersistentModel.Type] {
+        [Project.self]
     }
+
+    @Model
+    final class Project: Identifiable {
+        
+        // MARK: - Properties
+        
+        /// Unique identifier for the project.
+        var id: UUID
+        
+        /// Project's name.
+        var name: String
+        
+        /// Datetime the project was created.
+        var created: Date
+        
+        /// Datetime the project was last updated.
+        var modified: Date
+        
+        /// Count value of the project.
+        var count: Int
+        
+        /// Color for the project.
+        var color: Int
+        
+        // MARK: - Initialization
+
+        init(id: UUID = UUID(), name: String, created: Date = Date(), modified: Date = Date(), count: Int = 0, color: Int) {
+            // Sanity check for proper values.
+            assert(count >= 0)
+            assert(color >= 0x000000 && color <= 0xffffff)
+            
+            self.id = id
+            self.name = name
+            self.created = created
+            self.modified = modified
+            self.count = count
+            self.color = color
+        }
+    }
+
+}
+
+extension Project {
     
     // MARK: - Public Interface
     
